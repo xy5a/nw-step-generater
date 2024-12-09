05df3124bf12cb0ff0d95ded2e5d1cb92b98b952
xy_5a <xy_5a@outlook.com>
2024-12-09 20:20:19 +0800

    initial the project

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+let {count, doubleCount, increment } = useCounterStore();
+
+</script>
+
+<template>
+    <div>
+        <h1>Counter</h1>
+        <p>{{ count }}</p>
+        <p>{{ doubleCount }}</p>
+        <button @click="increment">Increment</button>
+    </div>
+
+</template>
+
+<style scoped>
+</style>
